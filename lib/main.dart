0276d4b7a053fbe75669033db7bfc4f6f0b5ed18
Felipe <felipe.bufelli@gmail.com>
2020-07-27 22:28:34 -0300

    Created project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:chat_real_time/screens/splash.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Splash()
+    );
+  }
+}
